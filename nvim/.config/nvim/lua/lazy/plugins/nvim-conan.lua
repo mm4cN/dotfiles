1c49ee1909ad4b7d433722f7c2c7ddc708acbfc7
Marek Macznik <marek.macznik@gmail.com>
2026-01-07 22:28:27 +0100

    Revert "Disable copilot for C/C++"
    This reverts commit b05996d18d41be65206182bb9606c3f9fdeb3d78.

--- nvim/.config/nvim/lua/lazy/plugins/nvim-conan.lua
+++ nvim/.config/nvim/lua/lazy/plugins/nvim-conan.lua
@@ -1,12 +1,7 @@
 return {
-  {
-    dir = "~/Projects/github/lua_workspace/nvim-conan",
-    name = "nvim-conan",
-    lazy = false,
-  }
-  -- "mm4cN/nvim-conan",
-  -- dependencies = {
-  --   "nvim-telescope/telescope.nvim",
-  -- },
+  "mm4cN/nvim-conan",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
 }
 
