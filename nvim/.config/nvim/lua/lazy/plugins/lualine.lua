052be0503900ea5e380e4f85ab3b322ab02e74f6
Marek Macznik <marek.macznik@gmail.com>
2026-02-24 01:09:01 +0100

    Add: conan status in statusline

--- nvim/.config/nvim/lua/lazy/plugins/lualine.lua
+++ nvim/.config/nvim/lua/lazy/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
     'yavorski/lualine-macro-recording.nvim',
   },
   config = function()
+    local conan_status = require("conan_status")
     require("lualine").setup({
       options = {
         icons_enabled = true,
@@ -13,7 +14,7 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { "filename", "filesize", "macro_recording", "%S" },
-        lualine_x = { "selectioncount", "searchcount", "encoding", "fileformat", "filetype" },
+        lualine_x = { conan_status.component, "selectioncount", "searchcount", "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       }
