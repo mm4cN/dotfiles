25408dabcf30ff51506e8cf8e72acca8c14b117a
Marek Macznik <marek.macznik@gmail.com>
2026-06-04 00:10:39 +0200

    Add: pull bigger qwen

--- post-install.sh
+++ post-install.sh
@@ -5,6 +5,7 @@ uv tool install platformio
 
 if command -v ollama >/dev/null 2>&1; then
   ollama pull qwen2.5-coder:3b
+  ollama pull qwen2.5-coder:7b
   ollama pull gemma3:4b
   ollama pull llama3.2:3b 
 else
