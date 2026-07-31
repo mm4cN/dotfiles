e44f0a9a0290cc5bac599515485d201ae7750cf6
Marek Macznik <marek.macznik@gmail.com>
2026-07-31 23:22:21 +0200

    remove nvm

--- home.nix
+++ home.nix
@@ -130,13 +130,8 @@ in
           --walker-skip .git,node_modules,target
           --preview 'tree -C {}'"
 
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-        [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-
         [[ -f ~/.auths ]] && source ~/.auths
         [[ -f "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
-
       ''
     ];
 
